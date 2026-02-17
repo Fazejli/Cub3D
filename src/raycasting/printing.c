23db33bcdf6dedad8913e7909d3831cf2992e3e5
M.S <ms@hadronlang.com>
2026-02-17 20:13:20 +0100

    fix: change integer types for more safety. Write to framebuffer instead of putting pixels

--- src/raycasting/printing.c
+++ src/raycasting/printing.c
@@ -6,72 +6,74 @@
 /*   By: fadwa <fadwa@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/12 12:20:10 by fadzejli          #+#    #+#             */
-/*   Updated: 2026/02/16 14:53:19 by fadwa            ###   ########.fr       */
+/*   Updated: 2026/02/17 20:12:31 by smamalig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+#include <stdint.h>
 
-void	print_ceiling(t_ray *ray, t_game *game, int col)
+void	print_ceiling(t_ray *ray, t_game *game, uint32_t x)
 {
-	int	color;
-	int	pixel;
+	uint32_t	color;
+	uint32_t	y;
 
 	color = game->data->ceiling_color;
-	pixel = 0;
-	while (pixel < ray->start)
+	y = 0U;
+	while (y < (uint32_t)ray->start)
 	{
-		if (pixel >= HEIGHT)
+		if (y >= HEIGHT)
 			break ;
-		mlx_pixel_put(game->mlx, game->win, col, pixel, color);
-		pixel++;
+		game->addr[y * WIDTH + x] = color;
+		y++;
 	}
 }
 
-void	print_floor(t_ray *ray, t_game *game, int col)
+void	print_floor(t_ray *ray, t_game *game, uint32_t x)
 {
-	int	color;
-	int	pixel;
+	uint32_t	color;
+	uint32_t	y;
 
 	color = game->data->floor_color;
-	pixel = ray->end + 1;
-	while (pixel < HEIGHT)
+	y = ray->end + 1;
+	while (y < HEIGHT)
 	{
-		mlx_pixel_put(game->mlx, game->win, col, pixel, color);
-		pixel++;
+		game->addr[y * WIDTH + x] = color;
+		y++;
 	}
 }
 
-void	print_texture(t_ray *ray, t_game *game, int col)
+void	print_texture(t_ray *ray, t_game *game, uint32_t x)
 {
 	t_texture	*tex;
-	int			i;
+	uint32_t	y;
 	double		step;
 	double		tex_pos;
-	int			color;
+	uint32_t	color;
 
-	i = ray->start;
+	y = ray->start;
 	tex = get_texture(ray, game);
-	step = (double)tex->height / (double)ray->line_height;
-	tex_pos = (ray->start - HEIGHT / 2 + ray->line_height / 2) * step;
-	while (i <= ray->end)
+	step = (float)tex->height / (float)ray->line_height;
+	tex_pos = (ray->start - (float)HEIGHT / 2
+			+ (float)ray->line_height / 2) * step;
+	tex->x = find_intersection(ray, game->player) * tex->width;
+	while (y <= (uint32_t)ray->end)
 	{
-		if (i >= HEIGHT)
+		if (y >= HEIGHT)
 			break ;
-		tex->x = find_intersection(ray, game->player) * tex->width;
 		tex->y = check_pos(tex_pos, tex);
 		color = get_color(tex);
-		mlx_pixel_put(game->mlx, game->win, col, i, color);
+		game->addr[y * WIDTH + x] = color;
 		tex_pos += step;
-		i++;
+		y++;
 	}
 }
 
-void	print_ray(t_ray *ray, t_game *game, int col)
+void	print_ray(t_ray *ray, t_game *game, int x)
 {
-	if (col < 0 || col >= WIDTH)
+	if (x < 0 || x >= WIDTH)
 		return ;
-	print_ceiling(ray, game, col);
-	print_floor(ray, game, col);
-	print_texture(ray, game, col);
+	print_ceiling(ray, game, x);
+	print_floor(ray, game, x);
+	print_texture(ray, game, x);
 }
